d0cb81751d0a7a75d9f00ffb0f425d0cf6a94900
lifengchao5 <lifengchao5@jd.com>
2022-06-06 08:03:28 +0800

    1

--- src/main/java/scala/sql/udf/internal/stringExpressions.scala
+++ src/main/java/scala/sql/udf/internal/stringExpressions.scala
@@ -1,12 +1,18 @@
 package scala.sql.udf.internal
 
+import org.apache.flink.table.api.DataTypes
 import org.apache.flink.table.catalog.DataTypeFactory
-import org.apache.flink.table.data.StringData
-import org.apache.flink.table.data.binary.{BinaryStringData, BinaryStringDataUtil}
-import org.apache.flink.table.types.DataType
+import org.apache.flink.table.data.{ArrayData, GenericArrayData, RowData, StringData}
+import org.apache.flink.table.data.binary.{BinaryArrayData, BinaryStringData, BinaryStringDataUtil}
+import org.apache.flink.table.types.{CollectionDataType, DataType}
+import org.apache.flink.table.types.logical.LogicalTypeRoot._
 import org.apache.flink.table.types.inference.{ArgumentCount, CallContext}
+import org.apache.flink.table.types.logical.{ArrayType, VarCharType}
 
+import scala.collection.JavaConverters._
 import InternalScalarFunction._
+import scala.annotation.varargs
+import scala.collection.Iterator
 
 class SubstringIndex extends InternalScalarFunction {
 
@@ -54,4 +60,40 @@ class SubstringIndex extends InternalScalarFunction {
     Seq(stringDateType, stringDateType, intDateType)
 
   override def inferOutputTypes(args: Seq[DataType], callContext: CallContext, typeFactory: DataTypeFactory): DataType = stringDateType
+}
+
+// org.apache.spark.sql.catalyst.expressions.ConcatWs
+class ConcatWs extends InternalScalarFunction{
+
+  @varargs
+  def eval(eles: AnyRef*): StringData = {
+    val flatInputs = eles.tail.toIterator.flatMap { ele =>
+      ele match {
+        case null => Iterator(null)
+        case s: StringData => Iterator(s)
+        case arr: ArrayData => arr match {
+          case array: GenericArrayData => array.toObjectArray.asInstanceOf[Array[StringData]]
+          case array: BinaryArrayData => array.toObjectArray[StringData](new VarCharType(Integer.MAX_VALUE))
+        }
+      }
+    }.asInstanceOf[Iterator[BinaryStringData]]
+
+    BinaryStringDataUtil.concatWs(eles(0).asInstanceOf[BinaryStringData], flatInputs.toIterable.asJava)
+  }
+
+  override def argumentCount: ArgumentCount = anyArgumentCount
+
+  override def stringArgs: Seq[String] = Seq("sep[, str | array(str)]")
+
+  override def inferInputTypes(args: Seq[DataType], callContext: CallContext): Seq[DataType] = {
+    args.map{ arg =>
+      if(arg.getLogicalType.getTypeRoot == ARRAY){
+        new CollectionDataType(new VarCharType(Integer.MAX_VALUE), DataTypes.STRING())
+      }else{
+        DataTypes.STRING()
+      }
+    }
+  }
+
+  override def inferOutputTypes(args: Seq[DataType], callContext: CallContext, typeFactory: DataTypeFactory): DataType = DataTypes.STRING()
 }
